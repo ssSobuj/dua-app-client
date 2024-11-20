0a78d837b3d5afc580d3fa9965bdfb1a108a6a88
ssSobuj <react.tork@gmail.com>
2024-11-20 22:44:27 +0600

    First Commit , client side initialized

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+const nextConfig = {};
+
+module.exports = {
+  images: {
+    domains: ["i.ibb.co"],
+  },
+};
+
+// export default nextConfig;
